66c9390f9d489966b78f4741475c133c307474f7
yuhj974 <y18378960325@163.com>
2025-05-16 21:54:53 +0800

    Delete vue3 directory

--- vue3/src/api/login.js
+++
@@ -1,9 +0,0 @@
-import request from "../utils/request";
-//登录
-export const login = (data) => {
-  return request.post("/login", data);
-};
-//用户注册
-export const register = (data) => {
-  return request.post("/register", data);
-};
