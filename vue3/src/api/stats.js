7bfe6477d7461a1997453ddc55d3a66a1078909a
yuhj974 <y18378960325@163.com>
2025-05-16 21:07:07 +0800

    Delete vue3 directory

--- vue3/src/api/stats.js
+++
@@ -1,8 +0,0 @@
-import request from "../utils/request";
-export const getStatsByDate = () => {
-  return request.get(`/stats/general`);
-};
-//获取分类统计数据
-export const getStatsByCategory = () => {
-  return request.get(`/stats/category`);
-};
