884c93bc37e108e0525a1ad5b8989c43e6e35c14
yuhj974 <y18378960325@163.com>
2025-05-16 21:10:13 +0800

    Delete bookserve directory

--- bookserve/src/main/java/com/bookserve/exception/CategoryHasBooksException.java
+++
@@ -1,7 +0,0 @@
-package com.bookserve.exception;
-
-public class CategoryHasBooksException extends RuntimeException {
-    public CategoryHasBooksException(String message) {
-        super(message);
-    }
-}
