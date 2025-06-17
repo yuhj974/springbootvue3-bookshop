0a4126531436c76454799acd153d61bddd198d05
yuhj974 <y18378960325@163.com>
2025-06-18 00:19:51 +0800

    Delete bookserve directory

--- bookserve/src/main/java/com/bookserve/utils/PasswordUtil.java
+++
@@ -1,9 +0,0 @@
-package com.bookserve.utils;
-
-import org.apache.commons.codec.digest.DigestUtils;
-
-public class PasswordUtil {
-    public static String md5Encrypt(String password) {
-        return DigestUtils.md5Hex(password);
-    }
-}
